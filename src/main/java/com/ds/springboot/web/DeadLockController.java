65252bd8081acd09476d31caae96cc00c3cf495e
dineshseralathan <dineshseralathan@gmail.com>
2017-09-23 19:22:44 -0500

    merges

--- src/main/java/com/ds/springboot/web/DeadLockController.java
+++ src/main/java/com/ds/springboot/web/DeadLockController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ds.springboot.domain.DeadLockObject;
 import com.ds.springboot.service.DeadLockInitiator;
@@ -28,6 +28,7 @@ public class DeadLockController {
 	
 	
 	@RequestMapping(method=RequestMethod.POST,value="/deadlock")
+	@ResponseBody
 	public DeadLockObject createDeadLock(@RequestBody  List<DeadLockObject> objList)
 	{   
 		return deadLockInitiator.initiateDeadLock(objList);
